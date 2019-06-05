dd9913f87ba1423cb09c5b414b6cf5b3a9170745
Devin Warrick <dev.warr@gmail.com>
2019-06-05 15:08:36 -0500

    Login.js looking good. handleChanges method typed out. Moving to laptop.

--- insta-clone/src/components/Login/Login.js
+++ insta-clone/src/components/Login/Login.js
@@ -3,34 +3,44 @@ import instagramWord from '../../assets/instagramWord.png';
 
 class Login extends React.Component {
     state ={
+        username: "",
+        password: "",
 
     }
 
 
     handleChanges = e => {
         e.preventDefault();
-        
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
+
 
     render() {
         return (
             <div className="login-page">
                 <div className="login-container">
                     <img src={instagramWord} alt="Instagram" />
-                    <form className="login-form">
+                    <form 
+                        className="login-form"
+                        onSubmit={this.login}
+                    >
                         <input 
                             className="username"
                             type="text"
                             name="username"
+                            value={this.state.username}
                             onChange={this.state.handleChanges}
                         />
                         <input 
                             className="password"
                             type="password"
                             name="password"
+                            value={this.state.password}
                             onChange={this.state.handleChanges}
                         />
-                        <button>Log in</button>
+                        <button type="submit">Log in</button>
                     </form>
                 </div>
             </div>
