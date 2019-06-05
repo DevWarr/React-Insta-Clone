827177144cb7fc2f7864992bbd941b226e2e64c3
Devin Warrick <dev.warr@gmail.com>
2019-06-05 17:05:37 -0500

    Login page styled nicely! Hoo rah.

--- insta-clone/src/components/Login/Login.js
+++ insta-clone/src/components/Login/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import instagramWord from '../../assets/instagramWord.png';
+import './Login.scss';
 
 class Login extends React.Component {
     state = {
         username: "",
         password: "",
-
     }
 
     login = e => {
@@ -27,6 +27,7 @@ class Login extends React.Component {
             <div className="login-page">
                 <div className="login-container">
                     <img src={instagramWord} alt="Instagram" />
+                    <h2>Log In</h2>
                     <form 
                         className="login-form"
                         onSubmit={this.login}
@@ -35,6 +36,7 @@ class Login extends React.Component {
                             className="username"
                             type="text"
                             name="username"
+                            placeholder="username..."
                             value={this.state.username}
                             onChange={this.handleChanges}
                         />
@@ -42,10 +44,11 @@ class Login extends React.Component {
                             className="password"
                             type="password"
                             name="password"
+                            placeholder="password..."
                             value={this.state.password}
                             onChange={this.handleChanges}
                         />
-                        <button type="submit">Log in</button>
+                        <button className={this.state.username ? "typing" : null} type="submit">Log in</button>
                     </form>
                 </div>
             </div>
